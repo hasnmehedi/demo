27a2f9fe8105131b9ccdc1c340994e039bec0a11
hasnmehedi <mehedihasan200516@gmail.com>
2025-02-24 19:45:02 +0000

    version 1- creating variables

---
+++ project1.py
@@ -0,0 +1,10 @@
+# mehedi hasan
+#project demo
+
+print('hello world')
+print('hello pythion world')
+
+
+a, b = 'hello', 'world'
+print(a, b, sep = '\n')
+print(a,'\t\t', 'python' .upper(), '\t',b)
